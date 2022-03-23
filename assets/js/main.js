045e3432e61ba5f2a7fd9997d6cbfa136e7c1764
Aleksandr Fominov <alexfo93@mail.ru>
2022-03-23 08:58:00 +0300

    slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,54 @@
-(function ($) {
-	// $('.questions-1').on('click', function () {
-	// 	if ($(this).parents('.text-questions').hasClass('active')) {
-	// 		$(this).parents('.text-questions').removeClass('active');
-	// 	} else {
-	// 		$('.questions-1').parents('.text-questions').removeClass('active');
-	// 		$(this).parents('.text-questions').addClass('active');
-	// 	}
-	// });
 
-	$('.text').on('click', function() {
-		$(this).toggleClass('active');
-		$('.text-questions').toggleClass('active');
+(function ($) {
+	$('.questions-1').on('click', function () {
+		if ($(this).parents('.text-questions').hasClass('active')) {
+			$(this).parents('.text-questions').removeClass('active');
+		} else {
+			$('.questions-1').parents('.text-questions').removeClass('active');
+			$(this).parents('.text-questions').addClass('active');
+		}
 	});
+
+
+	$('.home-slider').slick({
+		arrows: false,
+		dots: false,
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		responsive: [
+			{
+			  breakpoint: 1024,
+			  settings: {
+				 slidesToShow: 3,
+				 slidesToScroll: 3,
+				 infinite: true,
+				 dots: true
+			  }
+			},
+			{
+			  breakpoint: 600,
+			  settings: {
+				 slidesToShow: 2,
+				 slidesToScroll: 2
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+				 slidesToShow: 2,
+				 slidesToScroll: 1,
+				 dots: true
+			  }
+			}
+		 ]
+	});
+
+
+	// $('.text').on('click', function() {
+	// 	$(this).toggleClass('active');
+	// 	$('.text-questions').toggleClass('active');
+	// });
 
 	// $('.btn').on('click', function() {
 
